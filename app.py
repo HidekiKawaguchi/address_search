230c06566429bcd12e169f0e2bde37156d6382e6
johnpori <h.kawaguchi0610@gmail.com>
2022-01-29 15:58:54 +0900

    関数他2種_完成

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 import requests
 
 
-def main():
-    zipcode = "7984101"
-    # zipcode = input("郵便番号<ハイフン無し7桁>は?")
+def search_address(zipcode):
     url = f"https://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}"
-
     response = requests.get(url)
     results = response.json()['results']
-
     pref_name = results[0]['address1']
     city_name = results[0]['address2']
     town_name = results[0]['address3']
-
     address = f"{pref_name}{city_name}{town_name}"
+    return address
+
+
+def main():
+    zipcode = "7984101"
+    # zipcode = input("郵便番号<ハイフン無し7桁>は?")
+
+    address = search_address(zipcode)
+
     print(address)
 
 
