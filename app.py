3b60a8a9f6c2808d2fc4c27d39939baf0de37bd0
johnpori <h.kawaguchi0610@gmail.com>
2022-01-29 16:18:50 +0900

    処理部分を別ファイルにまとめた

--- app.py
+++ app.py
@@ -1,20 +1,9 @@
-import requests
-
-
-def search_address(zipcode):
-    url = f"https://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}"
-    response = requests.get(url)
-    results = response.json()['results']
-    pref_name = results[0]['address1']
-    city_name = results[0]['address2']
-    town_name = results[0]['address3']
-    address = f"{pref_name}{city_name}{town_name}"
-    return address
+from search_address import search_address
 
 
 def main():
-    zipcode = "7984101"
-    # zipcode = input("郵便番号<ハイフン無し7桁>は?")
+    # zipcode = "7984101"
+    zipcode = input("郵便番号<ハイフン無し7桁>は?")
 
     address = search_address(zipcode)
 
