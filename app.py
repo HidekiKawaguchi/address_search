1b3e90d6cf3f1fcb18f0ea7c22cf47a214dfe3bf
johnpori <h.kawaguchi0610@gmail.com>
2022-01-29 15:42:31 +0900

    main関数完成

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 import requests
 
-zipcode = "7984101"
-# zipcode = input("郵便番号<ハイフン無し7桁>は?")
-url = f"https://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}"
 
-response = requests.get(url)
-results = response.json()['results']
+def main():
+    zipcode = "7984101"
+    # zipcode = input("郵便番号<ハイフン無し7桁>は?")
+    url = f"https://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}"
 
-pref_name = results[0]['address1']
-city_name = results[0]['address2']
-town_name = results[0]['address3']
+    response = requests.get(url)
+    results = response.json()['results']
 
-address = f"{pref_name}{city_name}{town_name}"
-print(address)
+    pref_name = results[0]['address1']
+    city_name = results[0]['address2']
+    town_name = results[0]['address3']
+
+    address = f"{pref_name}{city_name}{town_name}"
+    print(address)
+
+
+if __name__ == '__main__':
+    main()
